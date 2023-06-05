a264c123f7ad270961501e61b5c544e9869f5058
“Abdul <kaazees31@gmail.com>
2023-06-05 20:00:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import * as dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./Config/db.js";
@@ -10,11 +12,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// const MONGO_URL = process.env.MONGO_URL;
+////DATABASE CONNECTION
+connectDB();
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-connectDB();
+app.use(cors());
+app.use(morgan("dev"))
 
 /////ROUTERS
 
@@ -22,7 +28,7 @@ app.use("/api/bike", BikeRouter);
 app.use("/api/product", Productbike);
 
 //home
-app.get("/home", function (request, response) {
+app.get("/", function (request, response) {
   response.send("<h1>Welcome to bile rental App</h1>");
 });
 
